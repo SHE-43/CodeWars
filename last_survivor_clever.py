fa45ec72c10bd440337a7b8699288db0985e164e
SHE-43 <69411606+SHE-43@users.noreply.github.com>
2021-09-29 13:47:19 +0100

    Delete last_survivor_clever.py

--- last_survivor_clever.py
+++
@@ -1,8 +0,0 @@
-from functools import reduce
-
-# Reduce returns 0 if there is no element in the list for y when lambda x:y is used.
-# When there is a list [1] used for lambda x:y, it will return 1.
-
-def last_survivor(string, coords):
-    return reduce(lambda str_,index_:f'{str_[0:index_]}{str_[index_ + 1:]}',[string]+coords)
-    
